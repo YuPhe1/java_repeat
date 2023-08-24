cace1c4dbb0442ee99cf706ad899a2683ad50c8a
YuPhe1 <oksunboy1@naver.com>
2023-08-25 08:42:32 +0900

    회원가입 로그인

---
+++ src/service/MemberService.java
@@ -0,0 +1,94 @@
+package service;
+
+import dto.MemberDTO;
+import repository.MemberRepository;
+
+import java.util.Scanner;
+
+public class MemberService {
+    MemberRepository memberRepository = new MemberRepository();
+    // 로그인 한 사용자 정보
+   private static MemberDTO loginMemberDTO = null;
+    Scanner scanner = new Scanner(System.in);
+    public void save(){
+        boolean checkEmail = false;
+        String memberEmail = null;
+        while (!checkEmail) {
+            System.out.print("이메일> ");
+            memberEmail = scanner.next();
+            checkEmail = memberRepository.checkEmail(memberEmail);
+            if(!checkEmail){
+                System.out.println("이미 사용중인 이메일입니다.");
+            }
+        }
+        System.out.print("비밀번호> ");
+        String memberPassword = scanner.next();
+        System.out.print("이름> ");
+        String memberName = scanner.next();
+        System.out.print("전화번호> ");
+        String memberPhone = scanner.next();
+        MemberDTO memberDTO = new MemberDTO(memberEmail,memberPassword,memberName,memberPhone);
+        if(memberRepository.save(memberDTO)){
+            System.out.println("회원가입성공");
+            System.out.println(memberDTO);
+        } else{
+            System.out.println("회원가입실패");
+        }
+    }
+
+    public void findAll(){
+        for(MemberDTO memberDTO : memberRepository.findAll()){
+            System.out.println(memberDTO);
+        }
+    }
+
+    public void logout() {
+        loginMemberDTO = null;
+    }
+
+    public boolean login() {
+        System.out.print("이메일> ");
+        String loginEmail = scanner.next();
+        System.out.print("비밀번호> ");
+        String loginPassword = scanner.next();
+        MemberDTO memberDTO = memberRepository.findByEmailAndPassword(loginEmail, loginPassword);
+        if(memberDTO == null){
+            System.out.println("로그인 실패");
+            return false;
+        } else {
+            System.out.println("로그인 성공");
+            loginMemberDTO = memberDTO;
+            return true;
+        }
+    }
+
+    public boolean delete() {
+        System.out.println("회원정보 : " + loginMemberDTO);
+        System.out.print("삭제하시겠습니까?  (y/n) > ");
+        String sel = scanner.next();
+        if(sel.equalsIgnoreCase("y")){
+           memberRepository.delete(loginMemberDTO.getId());
+           return false;
+        } else {
+            System.out.println("탈퇴를 취소합니다.");
+            return true;
+        }
+    }
+
+    public void update() {
+        System.out.print("이메일> ");
+        String memberEmail = scanner.next();
+        System.out.print("비밀번호> ");
+        String memberPassword = scanner.next();
+        if(loginMemberDTO.getMemberEmail().equals(memberEmail)
+                && loginMemberDTO.getMemberPassword().equals(memberPassword)){
+            System.out.println("회원정보 : " + loginMemberDTO);
+            System.out.print("수정할 전화번호> ");
+            String updateMobile = scanner.next();
+            memberRepository.update(loginMemberDTO.getId(), updateMobile);
+        } else {
+            System.out.println("이메일 또는 비밀번호가 틀렸습니다.");
+        }
+
+    }
+}
